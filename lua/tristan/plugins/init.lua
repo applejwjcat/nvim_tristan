90e388bd2e27a84428822180a6211e000323c3be
tristan <15997232823@163.com>
2024-09-15 17:10:59 +0800

    add the build command for semshi

--- lua/tristan/plugins/init.lua
+++ lua/tristan/plugins/init.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 	"numirias/semshi",
+	build = ":UpdateRemotePlugins",
 	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
