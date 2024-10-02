2e98187a001c73bb9534a211a54d845c02995c9c
tristan <15997232823@163.com>
2024-10-02 22:55:23 +0800

    changed comment keymap in which-key config2

--- lua/tristan/plugins/which-key.lua
+++ lua/tristan/plugins/which-key.lua
@@ -5,27 +5,23 @@ return {
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
 	end,
-	config = function()
-		local wk = require("which-key")
-		wk.setup({})
-		wk.add({
-			{
-				"<leader>/",
-				mode = "n",
-				"<Plug>(comment_toggle_linewise_current)",
-				desc = "Toggle comment line",
-			},
-			{
-				"<leader>/",
-				mode = "v",
-				"<Plug>(comment_toggle_linewise_visual)",
-				desc = "Toggle visual block",
-			},
-		})
-	end,
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>/",
+			mode = "n",
+			"<Plug>(comment_toggle_linewise_current)",
+			desc = "Toggle comment line",
+		},
+		{
+			"<leader>/",
+			mode = "v",
+			"<Plug>(comment_toggle_linewise_visual)",
+			desc = "Toggle visual block",
+		},
 	},
 }
