678d4d602e6afd9d88fb6db7b09a67f23862a625
tristan <15997232823@163.com>
2024-10-04 20:00:12 +0800

    add the visual-text

--- lua/tristan/plugins/debugger.lua
+++ lua/tristan/plugins/debugger.lua
@@ -9,10 +9,15 @@ return {
 					"nvim-neotest/nvim-nio",
 				},
 			},
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				commit = "9578276dfdc5420624699744377ddd74be2d9a19",
+			},
 		},
 		config = function()
 			local dap = require("dap")
 			local dapui = require("dapui")
+			require("nvim-dap-virtual-text").setup()
 			dapui.setup()
 			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
@@ -35,7 +40,7 @@ return {
 
 			vim.api.nvim_set_keymap(
 				"n",
-				"<leader>dpr",
+				"<leader>dr",
 				"<cmd>lua require('dap-python').test_method()<CR>",
 				{ noremap = true, silent = true }
 			)
@@ -45,61 +50,6 @@ return {
 				"<cmd>lua require('dap').continue()<CR>",
 				{ noremap = true, silent = true }
 			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>ds",
-				"<cmd>lua require('dap').step_over()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>di",
-				"<cmd>lua require('dap').step_into()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>do",
-				"<cmd>lua require('dap').step_out()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>dt",
-				"<cmd>lua require('dap').toggle_breakpoint()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>dl",
-				"<cmd>lua require('dap').run_last()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>dr",
-				"<cmd>lua require('dap').repl.toggle()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>dn",
-				"<cmd>lua require('dap').run()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>dp",
-				"<cmd>lua require('dap').pause.toggle()<CR>",
-				{ noremap = true, silent = true }
-			)
-			vim.api.nvim_set_keymap(
-				"n",
-				"<leader>de",
-				"<cmd>lua require('dap').disconnect()<CR>",
-				{ noremap = true, silent = true }
-			)
-
 			vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
 			vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
 			vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#ffffff", bg = "#FE3C25" })
